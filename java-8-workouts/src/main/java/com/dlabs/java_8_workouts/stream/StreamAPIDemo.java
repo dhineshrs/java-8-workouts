96d7889c064990743d00c6cf475c5012d5fd260a
Dhinesh <dhinesh.rs@gmail.com>
2018-09-27 17:35:09 +0530

    java 8 workouts

---
+++ java-8-workouts/src/main/java/com/dlabs/java_8_workouts/stream/StreamAPIDemo.java
@@ -0,0 +1,14 @@
+package com.dlabs.java_8_workouts.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamAPIDemo {
+
+	public static void main(String[] args) {
+		List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+		myList.stream().filter(s -> s.startsWith("c")).map(String::toUpperCase).sorted().forEach(System.out::println);
+	}
+
+}
